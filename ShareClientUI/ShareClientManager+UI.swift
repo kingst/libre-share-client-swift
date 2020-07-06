07f0a8957b873b6e52e86cc08ad16ebd495a38d3
Nathaniel Hamming <nate@hmtconsulting.org>
2020-07-06 14:55:43 -0300

    nate/feature/LOOP-1293/display-mock-cgm-status-progress (#12)
    * refactor to device status progress
    
    * renaming request

--- ShareClientUI/ShareClientManager+UI.swift
+++ ShareClientUI/ShareClientManager+UI.swift
@@ -31,7 +31,7 @@ extension ShareClientManager: CGMManagerUI {
     }
     
     // TODO Placeholder. This functionality will come with LOOP-1311
-    public var progressPercentCompleted: Double? {
+    public var cgmLifecycleProgress: DeviceLifecycleProgress? {
         return nil
     }
 }
